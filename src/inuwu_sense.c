dd382083dd79f4e82f11309a08548e007eb34c3e
notdonutegg-wq <notdonutegg@gmail.com>
2026-09-14 14:33:38 -0500

    Replace strncpy with memcpy for compatibility with newer arch versions
    Replace strncpy with memcpy for compatibility with newer arch versions

--- src/inuwu_sense.c
+++ src/inuwu_sense.c
@@ -3299,7 +3299,7 @@ enum acer_wmi_predator_v4_oc {
      char *token;
      char* input_ptr = input;
      size_t len = min(count, sizeof(input) - 1);
-     strncpy(input, buf, len);
+     memcpy(input, buf, len);
  
      if(input[len-1] == '\n'){
          input[len-1] = '\0';
@@ -3750,7 +3750,7 @@ enum acer_wmi_predator_v4_oc {
      char *input_ptr = input_buf;
      size_t len = min(count, sizeof(input_buf) - 1);
  
-     strncpy(input_buf, buf, len);
+     memcpy(input_buf, buf, len);
  
      if(input_buf[len-1] == '\n'){
          input_buf[len-1] = '\0';
@@ -3923,7 +3923,7 @@ enum acer_wmi_predator_v4_oc {
      struct per_zone_color colors;
      char *input_ptr = str_buf;
      len = min(count, sizeof(str_buf) - 1);
-     strncpy(str_buf, buf, len);
+     memcpy(str_buf, buf, len);
      if(str_buf[len-1] == '\n'){
          str_buf[len-1] = '\0';
      } else {
